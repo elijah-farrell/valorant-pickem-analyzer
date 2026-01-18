7382a4827b779fae59fa3bdeec62af8948478646
Elijah Farrell <elijah5003@gmail.com>
2026-01-18 13:25:22 -0500

    Changed API due to previous one capping at 50 players. Now get entire udnerdog slate no matter the size.

--- scraper/underdog.py
+++ scraper/underdog.py
@@ -1,11 +1,12 @@
 import requests
 
-def get_pickem_slate(sport_id="VAL"):
-    url = f"https://api.underdogfantasy.com/v2/pickem_search/search_results?sport_id={sport_id}"
+BASE_URL = "https://api.underdogfantasy.com/v1/over_under_lines?sport_id=val"
+
+def get_pickem_slate():
+    url = BASE_URL
     try:
-        response = requests.get(url)
+        response = requests.get(url, timeout=20)
         response.raise_for_status()
-        return response.json()  # Return full response, not just over_under_lines
+        return response.json()
     except requests.RequestException:
         return {}
-
